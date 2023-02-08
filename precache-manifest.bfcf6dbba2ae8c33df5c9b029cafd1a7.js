45571fe2a4ceaeb1e4a36b4cdad5f71b286d3150
bhargava16623 <105556187+bhargava16623@users.noreply.github.com>
2023-02-08 18:48:52 +0530

    Updates

---
+++ precache-manifest.bfcf6dbba2ae8c33df5c9b029cafd1a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c80843b17b65a6af8dd",
+    "url": "/textutils-react/static/css/main.11062801.chunk.css"
+  },
+  {
+    "revision": "9c80843b17b65a6af8dd",
+    "url": "/textutils-react/static/js/main.9c80843b.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/textutils-react/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/textutils-react/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "3e897f2210a78e65956d",
+    "url": "/textutils-react/static/js/runtime~main.3e897f22.js"
+  },
+  {
+    "revision": "2b29486fadf73adf1b9f51d015d7312e",
+    "url": "/textutils-react/index.html"
+  }
+];
